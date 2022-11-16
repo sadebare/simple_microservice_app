34c49d0349dbe41e37484fd978bbf9bb598d775d
sadebare <adebaresulaimon04@gmail.com>
2022-11-16 02:31:09 +0100

    microservice application

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express =  require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+  res.json({"content": ["TALENT PLUS",]})
+})
+
+app.get("/api", (req, res) => {
+   res.json({"content": ["TALENT PLUS",]})
+})
+
+app.listen(5000, () => {console.log("Server started on port 5000")})
